74b2eb31576e22f1fcd42dc8f09f11397e3393df
chithambarams <chithambaram.s@pickzy.com>
2019-03-14 22:10:22 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS `krds_poc`.`todo_list` (
 ID INT(11) AUTO_INCREMENT PRIMARY KEY,
 todo_list TEXT NOT NULL,
 status INT(11),
-date_created DATETIME,
-date_completed DATETIME)
+date_created DATETIME)
 ENGINE = InnoDB;
